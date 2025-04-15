1ffac41b8d4b41fee33141691052fc3e69bc44e9
ssharif2 <ssharif2@cmc.edu>
2025-04-15 14:28:53 -0700

    fixed so that the two tests should pass

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -5,23 +5,23 @@ files=$(find data/*)
 echo '================================================================================'
 echo 'load denormalized'
 echo '================================================================================'
-#time 
-for file in $files; do
-    zcat $file | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:1067 -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+time for file in $files; do
+	echo
+	unzip -p $file | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:1067 -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 done
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-#time 
-for file in $files; do
+time for file in $files; do
+    echo
     python3 load_tweets.py --db postgresql://postgres:pass@localhost:2067 --input "$file"
 done
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-#time 
-for file in $files; do
+time for file in $files; do
+    echo
     python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3067/ --inputs $file
 done
